118ffe92c9a0fdf33a4593a7e881b311a3b1fce1
viniciusbacellar <dev_bacellar@outlook.com>
2021-08-22 14:43:13 -0300

    otimização no requisições.js

--- js/requisicoes.js
+++ js/requisicoes.js
@@ -1,44 +1,29 @@
 
 function brazil () {
-  const acumulado = document.querySelector('.acumulado')
   fetch('https://covid19-brazil-api.vercel.app/api/report/v1/brazil/')
     .then(r => r.json())
     .then(totalBrazil => {
+      const acumulado = document.querySelector('.acumulado')
+      const obitoAcumulado = document.querySelector('.obito-acumulado')
+      const recuperados = document.querySelector('.recuperados')
+
       acumulado.innerHTML = (totalBrazil.data.confirmed).toLocaleString('pt-BR')
+      obitoAcumulado.innerHTML = (totalBrazil.data.deaths).toLocaleString('pt-BR')
+      recuperados.innerHTML = (totalBrazil.data.confirmed - totalBrazil.data.deaths).toLocaleString('pt-BR')
     })
 }
 brazil()
 
-function brazilObitos () {
-  const obitoAcumulado = document.querySelector('.obito-acumulado')
-  fetch('https://covid19-brazil-api.vercel.app/api/report/v1/brazil/')
+function estados () {
+  fetch('https://covid19-brazil-api.now.sh/api/report/v1')
     .then(r => r.json())
-    .then(totalBrazil => {
-      obitoAcumulado.innerHTML = (totalBrazil.data.deaths).toLocaleString('pt-BR')
-    })
-}
-brazilObitos()
-
-function casosRecuperados () {
-  const recuperados = document.querySelector('.recuperados')
-  fetch('https://covid19-brazil-api.vercel.app/api/report/v1/brazil/')
-    .then(r => r.json())
-    .then(totalBrazil => {
-      recuperados.innerHTML = (totalBrazil.data.confirmed - totalBrazil.data.deaths).toLocaleString('pt-BR')
-    })
-}
-casosRecuperados()
-
-function acompanhamento () {
-  const acompanhamento = document.querySelector('.acompanhamento')
-  fetch('https://covid19-brazil-api.vercel.app/api/report/v1')
-    .then(r => r.json())
-    .then(totalBrazil => {
-      const teste = totalBrazil.data
+    .then(estadosBrazil => {
+      const acompanhamento = document.querySelector('.acompanhamento')
+      const teste = estadosBrazil.data
       const somaAcom = teste.reduce((acumulador, item) => {
         return acumulador + item.suspects
       }, 0)
       acompanhamento.innerHTML = somaAcom.toLocaleString('pt-BR')
     })
 }
-acompanhamento()
+estados()
